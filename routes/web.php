4dc37d71c7243431cc6aed06081cd41ecfda6e82
TheVarja <40161420+TheVarja@users.noreply.github.com>
2024-06-05 13:29:00 +0700

    Commit new file

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('abou   ');
+});
+
+Route::get('/mahasiswa', function () {
+    echo "halaman Mahasiswa";
+});
+
+Route::get('/user/{id}', function($id){
+    echo "halaman user: $id";
+});
+
+Route::get('/users/{name?}', function ($name = 'Irvan'){
+    echo "halaman user $name";
+});
+
+Route::get('/post/{post}/comments/{comment}', function ($postId,$commentId) {
+    echo "post id : $postId, comment id : $commentId";
+});
+
+Route::get('/userss/{id}', function($id){
+   echo "halaman user: $id";
+})->where ('id', '[0-9]+');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
